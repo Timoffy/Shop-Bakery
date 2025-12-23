39899bbb2b815d22568eddd6512dc76d4de1375b
Timoffy <spacek778@gmail.com>
2025-12-23 19:47:37 +0100

    This is final verion of this website

---
+++ Bakery/js/swiper.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper-container', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.carusel-button.next',
+    prevEl: '.scarusel-button.prev',
+  },
+});
